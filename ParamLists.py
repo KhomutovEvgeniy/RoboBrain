29577a3f6c9d6068bdad6728ec46cdde458f820e
yelloy <umedamir@gmail.com>
2017-07-11 21:15:51 +0300

    New changed

--- ParamLists.py
+++ ParamLists.py
@@ -1,10 +1,8 @@
-### Описание параметров контроллеров в виде словарей
+### Описание параметров контроллеров в виде словарей, а так же функции упаковки сообщения в тип can message
 import can
 import struct
 
-def vacuum(self):
-    pass
-
+'''
 def MotorSpeedHandler(self):
     typeSpeed = struct.Struct('=2b h')              # Структура сообщения
     packedData = typeSpeed.pack(self.motorNumber+10, 2, self.speed)  # Упаковываем сообщение
@@ -13,12 +11,42 @@ def MotorSpeedHandler(self):
     packedMessage = can.Message(arbitration_id = self.CanAddr,
                                 extended_id = False,       
                                 data = packedData)
-    
+    return packedMessage    # Возвращаем упакованное сообщение
+'''
+
+def Int16Handler(self):
+    typeSpeed = struct.Struct('=2b h')              # Структура сообщения
+    packedData = typeSpeed.pack(self.prmNumber, 2, self.data)  # Упаковываем сообщение
+
+    # extended_id - фолс - 11 бит, тру - 29
+    packedMessage = can.Message(arbitration_id = self.CanAddr,
+                                extended_id = False,       
+                                data = packedData)
     return packedMessage    # Возвращаем упакованное сообщение
 
-def 2bh_Handler(self):
-    typeSpeed = struct.Struct('=2b h')              # Структура сообщения
-    packedData = typeSpeed.pack(self.prmNumber+10, 2, self.prm)  # Упаковываем сообщение
+def UInt16Handler(self):
+    typeSpeed = struct.Struct('=2b H')              # Структура сообщения
+    packedData = typeSpeed.pack(self.prmNumber, 2, self.data)  # Упаковываем сообщение
+
+    # extended_id - фолс - 11 бит, тру - 29
+    packedMessage = can.Message(arbitration_id = self.CanAddr,
+                                extended_id = False,       
+                                data = packedData)
+    return packedMessage    # Возвращаем упакованное сообщение
+
+def UInt8Handler(self):
+    typeSpeed = struct.Struct('=2b B')              # Структура сообщения
+    packedData = typeSpeed.pack(self.prmNumber, 1, self.data)  # Упаковываем сообщение
+
+    # extended_id - фолс - 11 бит, тру - 29
+    packedMessage = can.Message(arbitration_id = self.CanAddr,
+                                extended_id = False,       
+                                data = packedData)
+    return packedMessage    # Возвращаем упакованное сообщение
+
+def Int32Handler(self):
+    typeSpeed = struct.Struct('=2b l')              # Структура сообщения
+    packedData = typeSpeed.pack(self.prmNumber, 4, self.data)  # Упаковываем сообщение
 
     # extended_id - фолс - 11 бит, тру - 29
     packedMessage = can.Message(arbitration_id = self.CanAddr,
@@ -27,33 +55,131 @@ def 2bh_Handler(self):
     
     return packedMessage    # Возвращаем упакованное сообщение
 
+def FloatHandler(self):
+    typeSpeed = struct.Struct('=2b f')              # Структура сообщения
+    packedData = typeSpeed.pack(self.prmNumber, 4, self.data)  # Упаковываем сообщение
+
+    # extended_id - фолс - 11 бит, тру - 29
+    packedMessage = can.Message(arbitration_id = self.CanAddr,
+                                extended_id = False,       
+                                data = packedData)
+    
+    return packedMessage    # Возвращаем упакованное сообщение
+
+def ByteHandler(self):
+    typeSpeed = struct.Struct('=2b B')              # Структура сообщения
+    packedData = typeSpeed.pack(self.prmNumber, 1, self.data)  # Упаковываем сообщение
+
+    # extended_id - фолс - 11 бит, тру - 29
+    packedMessage = can.Message(arbitration_id = self.CanAddr,
+                                extended_id = False,       
+                                data = packedData)
+    
+    return packedMessage    # Возвращаем упакованное сообщение
+
+def NoneHandler(self):
+    typeSpeed = struct.Struct('=2b')              # Структура сообщения
+    packedData = typeSpeed.pack(self.prmNumber, 0)  # Упаковываем сообщение
+
+    # extended_id - фолс - 11 бит, тру - 29
+    packedMessage = can.Message(arbitration_id = self.CanAddr,
+                                extended_id = False,       
+                                data = packedData)
+    
+    return packedMessage    # Возвращаем упакованное сообщение
+
+def ByteShortHandler(self):
+    typeSpeed = struct.Struct('=2b B h')              # Структура сообщения
+    packedData = typeSpeed.pack(self.prmNumber, 3, self.prm, self.prm2)  # Упаковываем сообщение
+
+    # extended_id - фолс - 11 бит, тру - 29
+    packedMessage = can.Message(arbitration_id = self.CanAddr,
+                                extended_id = False,       
+                                data = packedData)
+    
+    return packedMessage    # Возвращаем упакованное сообщение
+
+)
+
+def ShortShortHandler(self):
+    typeSpeed = struct.Struct('=2b h h')              # Структура сообщения
+    packedData = typeSpeed.pack(self.prmNumber, 4, self.prm, self.prm2)  # Упаковываем сообщение
+
+    # extended_id - фолс - 11 бит, тру - 29
+    packedMessage = can.Message(arbitration_id = self.CanAddr,
+                                extended_id = False,       
+                                data = packedData)
+    
+    return packedMessage    # Возвращаем упакованное сообщение
+
+)
+
 
     
 # ControllerMotor
-ParamListMC = {'0xc8':{'help':'work mode', 'handler': None},
-               '0xc9':{'help':'send parameters', 'handler': None},
-               '0xca':{'help':'write in EEPROM', 'handler': None},
-               '0xcb':{'help':'read from EEPROM', 'handler': None},
-               '0xcc':{'help':'clear odometres', 'handler': None},
-               '0xcd':{'help':'set PWM for same time', 'handler': None},
-               '0xce':{'help':'set PWM', 'handler': None},
-               '0xcf':{'help':'set speed', 'handler': MotorSpeedHandler},
-               '0xd0':{'help':'set all PWM', 'handler': None},
-               '0xd1':{'help':'set all speeds', 'handler': None},
-               '0xd2':{'help':'power out', 'handler': None},
+ParamListMC = {
+                        # Команды управления
+
+               '0xc8':{'name':'work mode', 'type': 'UInt8'}, # пример
+               '0xc9':{'help':'send parameters', 'handler': NoneHandler},
+               '0xca':{'help':'write in EEPROM', 'handler': NoneHandler},
+               '0xcb':{'help':'read from EEPROM', 'handler': NoneHandler},
+               '0xcc':{'help':'clear odometres', 'handler': NoneHandler},
+               '0xcd':{'help':'set PWM for same time', 'handler': None},    # Не известно
+               '0xce':{'help':'set PWM', 'handler': UInt8Int16Handler},
+               '0xcf':{'help':'set speed', 'handler': UInt8Int16Handler},
+               '0xd0':{'help':'set all PWM', 'handler': Int16Int16Handler},
+               '0xd1':{'help':'set all speeds', 'handler': Int16Int16Handler},
+               '0xd2':{'help':'power out', 'handler': UInt8Handler},
+
+                        # Параметры для чтения и записи 
+
+               '0x00':{'help':'debug info mask', 'handler': UInt8Handler},
+               '0x01':{'help':'proportional coefficient', 'handler': FloatHandler},
+               '0x02':{'help':'integral coefficient', 'handler': FloatHandler},
+               '0x03':{'help':'derivative coefficient', 'handler': FloatHandler},
+               '0x04':{'help':'limit summ', 'handler': Int16Handler},
+               '0x05':{'help':'time PID', 'handler': UInt16Handler},
+               '0x06':{'help':'time PWM', 'handler': UInt16Handler},
+               '0x07':{'help':'pwm DeadZone', 'handler': UInt8Handler},
+               '0x08':{'help':'accelbreak step', 'handler': UInt8Handler},
+               '0x09':{'help':'emergency level', 'handler': UInt16Handler},
+
+                       # Параметры только для чтения
+
+               '0x13':{'help':'Error Code', 'handler': UInt8Handler},
+               '0x14':{'help':'Work mode', 'handler': UInt8Handler},
+
+               # 1 
+               
+               '0x15':{'help':'pParrot1', 'handler': FloatHandler},
+               '0x16':{'help':'iParrot1', 'handler': FloatHandler},
+               '0x17':{'help':'dParrot1', 'handler': FloatHandler},
+               '0x18':{'help':'res PWM1', 'handler': Int16Handler},
+               '0x19':{'help':'current Parrot1', 'handler': Int16Handler},
+               '0x1a':{'help':'encoder Data1', 'handler': Int32Handler},
+               '0x1b':{'help':'Int summ1', 'handler': Int16Handler},
+               '0x1c':{'help':'adc Average1', 'handler': UInt16Handler},
+               '0x1d':{'help':'set Parrot1', 'handler': Int16Handler},
+               '0x1e':{'help':'set PWM1', 'handler': Int16Handler},
+
+               # 2 
+               
+               '0x1f':{'help':'pParrot2', 'handler': FloatHandler},
+               '0x20':{'help':'iParrot2', 'handler': FloatHandler},
+               '0x21':{'help':'dParrot2', 'handler': FloatHandler},
+               '0x22':{'help':'res PWM2', 'handler': Int16Handler},
+               '0x23':{'help':'current Parrot2', 'handler': Int16Handler},
+               '0x24':{'help':'encoder Data2', 'handler': Int32Handler},
+               '0x25':{'help':'Int summ2', 'handler': Int16Handler},
+               '0x26':{'help':'adc Average2', 'handler': UInt16Handler},
+               '0x27':{'help':'set Parrot2', 'handler': Int16Handler},
+               '0x28':{'help':'set PWM2', 'handler': Int16Handler},
+               
               }
 
 # ControllerStepper
 ParamListSC = {'1':{'help':'asdasdas', 'value':123}}
-
-def Handlerissimo(self):
-
-    if self.CanAddr == 0x200:
-        return ParamListMC[hex(self.prmNumber)]['handler'](self)
-
-    if self.CanAddr == 0x230:
-        return ParamListSC[str(self.prmNumber)]['handler'](self)
-
 
 
 ## ВНИМАНИЕ
--- RoboMind.py
+++ RoboMind.py
@@ -3,10 +3,25 @@ import time
 import threading
 import struct
 import sys
-from ParamLists import *
 
 # Индикатор, в случае ошибки завершающий отправку онлайнов
 crash = False
+
+
+# Нумерация типов
+DT_NONE = 0
+DT_UINT8 = 1
+DT_INT8 = 2
+DT_UINT16 = 3
+DT_INT16 = 4
+DT_UINT32 = 5
+DT_INT32 = 6
+DT_FLOAT = 7
+DT_STRING = 8
+DT_PTR = 9
+DT_BITMASK = 10 # битовая маска,
+DT_RAW = 11 # просто байты
+                
 
 #### KOSTYIL PRODUCTION
 
@@ -44,19 +59,125 @@ class ControllerBase():
 
     def __init__(self, canAddr):
 
-        # Лист параметров
+        # Список параметров
         self.ParamList = {}
+        
+        # Список команд
+        self.CommandList = {}
 
         # Адресс устройства
         self.CanAddr = canAddr
-        
+
         self.exit = False
         
         # Подключаемся к шине
         self.Bus = can.interface.Bus(channel = 'can0', bustype = 'socketcan_native')  
 
+        self.StartRecv()    # Запускаем поток прослушивания шины 
+
     # Работа с парамлистом
+
+    def GetStructParam(self, prmType):    # Выявляем длину и структуру параметра запрошенного типа
+
+        prmLength = -1
+        
+        if prmType == (DT_UINT8 or DT_BITMASK):
+            prmStruct = struct.Struct('=2b B')
+            prmLength = 1
+                
+        elif prmType == DT_INT8:
+            prmStruct = struct.Struct('=2b b')
+            prmLength = 1
+
+        elif prmType == DT_UINT16:
+            prmStruct = struct.Struct('=2b H')
+            prmLength = 2
+
+        elif prmType == DT_INT16:
+            prmStruct = struct.Struct('=2b h')
+            prmLength = 2
+
+        elif prmType == DT_UINT32:
+            prmStruct = struct.Struct('=2b L')
+            prmLength = 4
+
+        elif prmType == DT_INT32:
+            prmStruct = struct.Struct('=2b l')
+            prmLength = 4
+
+        elif prmType == DT_FLOAT:
+            prmStruct = struct.Struct('=2b f')
+            prmLength = 4
+
+        else:
+            print('Неизвестный тип параметра:%d' % prmType)
+            # нужно прервать функцию
+        
+        '''elif prmType == DT_STRING:
+            prmStruct = struct.Struct('=2b p')
+            prmLength = 0
+
+        elif prmType == DT_RAW:
+            prmStruct = struct.Struct('=2b p')
+            prmLength = 0
+
+        elif prmType == DT_NONE:
+            print('ERROR: prmType = DT_NONE')'''
+            
+        return(prmStruct, prmLength)
+
+
+    def SetParam(self, prmNumber, value):   # Отправляем параметр на контроллер
+        if self.ParamList.get(prmNumber) != None:  # Существует ли параметр в списке параметров
+            prmType = self.ParamList[prmNumber][1]
+            prmStruct, prmLength = self.GetStructParam(prmType) # Формируем структуру и длину параметра
+
+            # Для параметров типа стринг, рав и нон
+            '''if prmLength == 0:  # Если значение длины параметра равно нулю, значит длину нужно определить особым способом 
+                if prmType = DT_STRING: # Если тип параметра строковый
+                    prmLength = len(value)
+                    if prmLength > 6: # Если в строке больше 6 символов
+                        value = value[:6] # Обрезаем строку до 6 символов
+                        prmLength = 6
+                    prmStruct = struct.Struct('=2b p')    
+                else:   # Когда prmType = DT_RAW
+            '''        
+                    
+                    
+            packedPrm = prmStruct.pack(prmNumber, prmLength, value)
+            packedMessage = can.Message(arbitration_id = self.CanAddr,
+                                        extended_id = False,       
+                                        data = packedPrm)
+
+            self.Send(packedMessage)
+        
+
+    # Обработчики
     
+    def UInt8Int16Handler(self):
+        datatype = struct.Struct('=2b B h')              # Структура сообщения
+        packedData = datatype.pack(self.prmNumber, 3, self.prm, self.prm2)  # Упаковываем сообщение
+
+        # extended_id - фолс - 11 бит, тру - 29
+        packedMessage = can.Message(arbitration_id = self.CanAddr,
+                                    extended_id = False,       
+                                    data = packedData)
+        
+        return packedMessage    # Возвращаем упакованное сообщение
+
+    
+    def Int16Int16Handler(self):
+        datatype = struct.Struct('=2b h h')              # Структура сообщения
+        packedData = datatype.pack(self.prmNumber, 4, self.prm, self.prm2)  # Упаковываем сообщение
+
+        # extended_id - фолс - 11 бит, тру - 29
+        packedMessage = can.Message(arbitration_id = self.CanAddr,
+                                    extended_id = False,       
+                                    data = packedData)
+        
+        return packedMessage    # Возвращаем упакованное сообщение
+
+
     # Отправка сообщения в шину
     def Send(self, msg):
         try:
@@ -76,23 +197,43 @@ class ControllerBase():
     # Функция запускаемая в потоке. Отвечает за прием сообщений
     def ThreadRecv(self):
         global crash
-        CanMsg = struct.Struct("=8H")  # Формат посылаемых сообщений        
 
         while not (self.exit or crash):
             inMsg = self.Recv()                      # Получение сообщения
-            self.recvMsg = CanMsg.unpack(inMsg.data) # Распаковка сообщения
 
+            if inMsg.arbitration_id == (self.CanAddr + 0xFF):
+                prmNumber = inMsg.data[0]
+
+                if self.ParamList.get(prmNumber) != None:  # Существует ли параметр в списке параметров
+                    prmLengthRecv = inMsg.data[1]
+
+                    if inMsg.dlc == prmLengthRecv + 2:  # Два байта занято номером параметра и значением длины
+                        prmStruct, prmLength = self.GetStructParam(self.ParamList[inMsg.data[0]][1])   # Выявление структуры и длины для данного типа сообщения
+
+                        if prmLength == prmLengthRecv:  # Длина принятого параметра соответствует длине параметра в парам листе 
+                            unpackedPrm = prmStruct.unpack(inMsg.data)
+                            try:
+                                self.ParamList[prmNumber][2] = unpackedPrm[2] # Пробуем записать полученное значение параметра в список параметров
+                            except:
+                                self.ParamList[prmNumber].append(unpackedPrm[2]) # Добавляем элемент в список параметров, если его еще не существует
+                            print('Param recieved. Name:%s  Value:%s' % (self.ParamList[prmNumber][0], str(self.ParamList[prmNumber][2])))
+                            
+                        else:
+                            print('Recieved length not according with recieving type')
+
+                    else:
+                        print('Recieved dlc not according with data length')
+
+
+                else:
+                    print('Попытка принять неизвестный параметр:%d' % prmNumber)
+                
     def StartRecv(self):
-        RecvThread = threading.Thread(target = ThreadRecv)
+        RecvThread = threading.Thread(target = self.ThreadRecv)
         RecvThread.start()
         self.StartRecv = self.Vacuum                
 
     # Для блокирования доступа к ф-ии присваиваем ей значение пустой функции
-
-    def Pack(self):                         # Упаковка сообщения
-        return Handlerissimo(self)
-        
-
     def Vacuum(self):
         pass
 
@@ -104,6 +245,7 @@ class ControllerBase():
 class ControllerMotor(ControllerBase):    
 
     def __init__(self, addr = 0):
+        print("addr: %d" % addr)
         try:
             # Возможные адреса лежат в диапазоне 0-8
             assert(0 <= addr < 9), ("ERROR adress controller")
@@ -113,7 +255,77 @@ class ControllerMotor(ControllerBase):
             print("Crashed")
             crash = True
             sys.exit(1)
-        
+
+        '''
+
+        CommandList = {
+                                # Команды управления
+
+                              #name, type, length
+                       0xC8:['work mode', dtUInt8], # пример. Переделать в списки. Название - тип. Параметр задавать не текстом, а номером.
+                       0xC9:['send parameters', dtNone, params],
+                       0xCA:['write in EEPROM', dtNone, startPositions],
+                       0xCB:['read from EEPROM', dtNone, startPositionsInEEPROM],
+                       0xCC:['clear odometres', dtNone, odometr],
+                       0xCD:['set PWM for same time', dtUnknown, timePWM],    # Не известно
+                       0xCE:['set PWM', dtUInt8Int16, PWM],
+                       0xCF:['set speed', dtInt8Int16, speed],
+                       0xD0:['set all PWM', dtInt16Int16, allPWM],
+                       0xD1:['set all speeds', dtInt16Int16, allSpeeds],
+                       0xD2:['power out', dtUInt8, powerStat],
+
+                       }
+                       '''
+
+
+        self.ParamList = {
+                                # Параметры для чтения и записи
+                                #name, type,(data)
+                       0x00:['debug info mask', DT_UINT8],
+                       0x01:['proportional coefficient', DT_FLOAT],
+                       0x02:['integral coefficient', DT_FLOAT],
+                       0x03:['derivative coefficient', DT_FLOAT],
+                       0x04:['limit summ', DT_INT16],
+                       0x05:['time PID', DT_UINT16],
+                       0x06:['time PWM', DT_UINT16],
+                       0x07:['pwm DeadZone', DT_UINT8],
+                       0x08:['accelbreak step', DT_UINT8],
+                       0x09:['emergency level', DT_UINT16],
+
+                               # Параметры только для чтения
+
+                       0x13:['Error Code', DT_UINT8],
+                       0x14:['Work mode', DT_UINT8],
+
+                       # 1 
+                       
+                       0x15:['pParrot1', DT_FLOAT],
+                       0x16:['iParrot1', DT_FLOAT],
+                       0x17:['dParrot1', DT_FLOAT],
+                       0x18:['res PWM1', DT_INT16],
+                       0x19:['current Parrot1', DT_INT16],
+                       0x1A:['encoder Data1', DT_INT32],
+                       0x1B:['Int summ1', DT_INT16],
+                       0x1C:['adc Average1', DT_UINT16],
+                       0x1D:['set Parrot1', DT_INT16],
+                       0x1E:['set PWM1', DT_INT16],
+
+                       # 2 
+                       
+                       0x1F:['pParrot2', DT_FLOAT],
+                       0x20:['iParrot2', DT_FLOAT],
+                       0x21:['dParrot2', DT_FLOAT],
+                       0x22:['res PWM2', DT_INT16],
+                       0x23:['current Parrot2', DT_INT16],
+                       0x24:['encoder Data2', DT_INT32],
+                       0x25:['Int summ2', DT_INT16],
+                       0x26:['adc Average2', DT_UINT16],
+                       0x27:['set Parrot2', DT_INT16],
+                       0x28:['set PWM2', DT_INT16],
+                       
+                      }
+
+      
     def Mode(self, workMode = 0):        # Инициализация режима работы (2 - ШИМ, 1 - включить ручной режим, 0 - пока используется как выключение ручного режима)
         
         initMessage = can.Message(arbitration_id = self.CanAddr, extended_id = False,      # extended_id - фолс - 11 бит, тру - 29 
@@ -121,16 +333,14 @@ class ControllerMotor(ControllerBase):
         self.Send(initMessage)
 
     def SetMotorSpeed(self, motorNumber, speed): # Установить скорость мотора (0 и 1)
-        self.motorNumber, self.speed, self.prmNumber = motorNumber, speed, 0xCF
-        
-        data = self.Pack()    # Пакуем сообщение, чтобы переслать его в корректном виде
+        prmNumber = 0xcf
+        data = self.Pack(prmNumber, motorNumber, speed)    # Пакуем сообщение, чтобы переслать его в корректном виде
         self.Send(data)  
 
 ################## КАК ЗАДАВАТЬ ШИМ? ############
     def SetMotorPWM(self, motorNumber, PWM): 
         pass
 #################################################
-
 
 
 ### Не используется в связи с не готовностью
